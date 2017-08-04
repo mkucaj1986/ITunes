050298f8373bc6d8cfb7a55e710e76f195600285
mkucaj <mkucaj86@gmail.com>
2017-08-04 10:46:23 +0200

    change to es6 class

--- dev/js/app/home/homeHeader.js
+++ dev/js/app/home/homeHeader.js
@@ -1,25 +1,26 @@
+/*jshint esversion: 6 */
+
 define([
     "jquery",
 
 ], function($) {
 
-    function HomeHeader() {
-        this.mainBox = document.querySelector("#App");
-        this.header = document.createElement("h1");
+    class HomeHeader {
+        constructor() {
+            this.mainBox = document.querySelector("#App");
+            this.header = document.createElement("h1");
+        }
+
+        init() {
+            var vm = this;
+            var App = document.querySelector(".App");
+            var headerTxt = 'iTunes App Ready';
+
+            App.appendChild(vm.header); 
+            vm.header.innerHTML = headerTxt;
+        }
     }
 
-    HomeHeader.prototype.render = function() {
-        var vm = this;
-        var App = document.querySelector(".App");
-        var headerTxt = 'iTunes App Ready';
-
-        App.appendChild(vm.header); 
-        vm.header.innerHTML = headerTxt;
-    };
-
-    HomeHeader.prototype.init = function() {
-        this.render();
-    };
-
     return new HomeHeader();
+
 });
--- dev/js/app/home/homeSearch.js
+++ dev/js/app/home/homeSearch.js
@@ -1,26 +1,26 @@
+/*jshint esversion: 6 */
+
 define([
     "jquery",
 
 ], function($) {
 
-    function homeSearch() {
-        this.searchWrapper = document.createElement("div");
-        this.searchBox = document.createElement("input");
+    class homeSearch {
+        constructor() {
+            this.searchWrapper = document.createElement("div");
+            this.searchBox = document.createElement("input");
+        }
+
+        init() {
+            var vm = this;
+            var App = document.querySelector(".App");
+
+            App.appendChild(vm.searchWrapper); 
+            vm.searchWrapper.className += "search-box-wrapp";
+            vm.searchBox.placeholder = "Type to find iTune..";
+            vm.searchWrapper.appendChild(vm.searchBox); 
+        }
     }
-
-    homeSearch.prototype.render = function() {
-        var vm = this;
-        var App = document.querySelector(".App");
-        
-        App.appendChild(vm.searchWrapper); 
-        vm.searchWrapper.className += "search-box-wrapp";
-        vm.searchBox.placeholder = "Type to find iTune..";
-        vm.searchWrapper.appendChild(vm.searchBox); 
-    };
-
-    homeSearch.prototype.init = function() {
-        this.render();
-    };
 
     return new homeSearch();
 });
