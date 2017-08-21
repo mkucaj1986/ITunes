0a2f97cfd5f79a18c909b1eecf5956011de5f6f2
mkucaj1986 <mkucaj86@gmail.com>
2017-08-21 11:23:58 +0200

    small build fix

--- build/rjsconfig.js
+++ build/rjsconfig.js
@@ -3,19 +3,9 @@ module.exports = {
   baseUrl: 'js/app',     
   dir: 'dist',             
   wrap: true,
-  modules: [
-    {
-      name: 'home/home'
-    }
-  ],
   //optimize: "none",
   fileExclusionRegExp: /(^build)|(.idea)|(gulpfile.js)|(gulp.sh)|(package.json)|(tmod)|(less)$/,
   optimizeCss: 'standard', /* none|standard|standard.keepLines|standard.keepComments|standard.keepComments.keepLines */
   removeCombined: true,
-
-  paths: {
-    jquery: "../lib/jquery"
-  },
-
   waitSeconds: 10
 };
