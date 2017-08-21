8c1f918bed34009e66b0f315b8f3a678aba878b1
mkucaj1986 <mkucaj86@gmail.com>
2017-08-21 11:21:54 +0200

    add redux dev tools

--- dev/js/app/common/redux/appStore.js
+++ dev/js/app/common/redux/appStore.js
@@ -1,8 +1,9 @@
 define('js/app/common/redux/appStore', [
     'js/lib/Redux/redux.min',
-    './appReducer'
-], function(redux, appReducer) {
-
+    './appReducer',
+    'js/lib/Redux/redux-devtools-extension/developmentOnly',
+], function(redux, appReducer, composeWithDevTools) {
+	const composeEnhancers = composeWithDevTools({ actionCreators });
     let store = redux.createStore(appReducer);
     return store;
 
